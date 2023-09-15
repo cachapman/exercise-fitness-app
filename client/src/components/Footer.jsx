66cff31a5a02a686534eaee93f11a792f425b7bb
Peter-JC-Wu <peterjcwu@gmail.com>
2023-09-15 04:18:43 -0400

    add copyright with automatic current year

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,8 @@ import { LinkContainer } from "react-router-bootstrap";
 import FooterLogo from "../assets/icons/human-bones.png";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <Navbar bg="dark" variant="dark" expand="lg" fixed="bottom" collapseOnSelect>
@@ -11,7 +13,7 @@ const Footer = () => {
             <LinkContainer to="/dashboard">
               <Nav.Link >
                 <img src={FooterLogo} alt="footer-logo" width="auto" height="55px" />
-                <Navbar.Text>Stronger Body and Mind Fitness by Wu</Navbar.Text>
+                <Navbar.Text>Stronger Body and Mind Fitness by Wu &copy;{currentYear}</Navbar.Text>
               </Nav.Link>
             </LinkContainer>
           </Nav>
