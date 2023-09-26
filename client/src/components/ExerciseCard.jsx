cef07a4d2c7a9fb61424a7d9cb304b33c2458f3a
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2023-09-26 21:34:06 +0200

    Error fixed

--- client/src/components/ExerciseCard.jsx
+++ client/src/components/ExerciseCard.jsx
@@ -9,20 +9,20 @@ import { toast } from "react-toastify";
 
 const ExerciseCard = ({ exercise, user, workout, setWorkout }) => {
 
-  const [clicked, setClicked] = useState(false);
 
+  
+  const [clicked, setClicked] = useState(false);
   const exerciseCardData = {
-    userId: user.userId,
+    userId: user?.userId,
     exercise: exercise,
   };
   // Verify correct data return in console
   console.log("exerciseCardData from ExerciseCard.jsx line 14: ", exerciseCardData);
   console.log("user from ExerciseCard.jsx line 10: ", user);
-  console.log("user.userId from ExerciseCard.jsx line 15: ", user.userId);
   console.log("exercise from ExerciseCard.jsx line 16: ", exercise);
 
   const exerciseCardParams = {
-    userId: user.userId,
+    userId: user?.userId,
     exerciseId: exercise.id,
   };
   // Verify correct data return in console
