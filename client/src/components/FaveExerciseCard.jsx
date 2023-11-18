dbd1658db0d790963553c2fb56388f62f55c68b5
Peter-JC-Wu <peterjcwu@gmail.com>
2023-11-18 06:39:20 -0500

    more CRUD functionality improvements

--- client/src/components/FaveExerciseCard.jsx
+++ client/src/components/FaveExerciseCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack, Tooltip, Typography } from "@mui/material";
-import { useDeleteSavedExerciseFromListMutation } from "../slices/usersApiSlice";
+import { useDeleteSavedExerciseFromListMutation, useFetchSavedFaveExercisesListQuery, useUpdateSavedFaveExercisesListMutation } from "../slices/usersApiSlice";
 import { removeSavedExerciseFromList } from "../slices/authSlice";
 import CheckIcon from "@mui/icons-material/Check";
 import { toast } from "react-toastify";
@@ -12,23 +12,24 @@ import Loader from "./Loader";
  * 
  * FaveExerciseCard is the grandchild component of FaveExercisesDashboard.
  * 
- * @param {Object} props - Props containing exercise, user, and onFilterAndSortChange.
- * @returns {JSX.Element} - A component for that sets the parameters for displaying the exercise card template.
+ * @param {Object} props - Props containing exercise, user, and onSortDisplayChange.
+ * @returns {JSX.Element} - A component for that sets the parameters for displaying the favorite exercise card template.
  */
 
-const FaveExerciseCard = ({ exercise, user, onFilterAndSortChange, onRemoveSavedExercise }) => {
+const FaveExerciseCard = ({ exercise, user, onSortDisplayChange }) => {
   // Get logged-in user information from Redux store
   // Initialize useDispatch to dispatch the save exercise action
   const dispatch = useDispatch();
   const exerciseId = exercise.exerciseId;
-  console.log("exerciseId at FaveExerciseCard.jsx: ", exercise.exerciseId);
 
   // Local state to track and initialize isLoading and visibility of the confirmation dialog state
   const [isLoading, setIsLoading] = useState(false);
   const [isConfirmationDialogOpen, setIsConfirmationDialogOpen] = useState(false);
 
-  // Use Mutation to interact with MongoDB
+  // Interact with MongoDB
   const [deleteExercise] = useDeleteSavedExerciseFromListMutation();
+  const [updateSavedExercisesList] = useUpdateSavedFaveExercisesListMutation();
+  const { refetch } = useFetchSavedFaveExercisesListQuery();
 
   // Functions to display the confirmation dialog
   const handleConfirmationDialogOpen = () => {
@@ -45,24 +46,29 @@ const FaveExerciseCard = ({ exercise, user, onFilterAndSortChange, onRemoveSaved
   };
 
   const removeExerciseFromSavedExerciseList = async (exerciseId) => {
+    console.log("Removing exercise started... at FaveExerciseCard.jsx");
+    
+    // Remove the saved exercise from saved favorite exercises list in MongoDB database
     setIsLoading(true);
-    // Remove the saved exercise from saved favorite exercises list
     try {
       await deleteExercise({ userId: user.id, exerciseId });
       // Dispatch the action to remove the exercise from savedExericseList
       dispatch(removeSavedExerciseFromList({ exerciseId: exerciseId }));
-      // Call the callback function to remove the exercise from the local state in parent component
-      onRemoveSavedExercise(exerciseId);
+      // Update the saved favorite exercises list
+      await updateSavedExercisesList();
+      // Manually refetch the data
+      refetch();
+      console.log("at FaveExerciseCard.jsx exercise ID remove:", exerciseId)
       toast.success("Exercise removed successfully from your saved favorite exercises list");
     } catch (err) {
-      toast.error(err.error);
+      toast.error(`Failed to remove exercise: ${err.error}`);
     } finally {
       setIsLoading(false);
     }
   };
 
-  const filterAndSort = (value) => {
-    onFilterAndSortChange({ target: { value } });
+  const sortDisplayOrderBy = (value) => {
+    onSortDisplayChange({ target: { value } });
   };
 
   return (
@@ -102,7 +108,7 @@ const FaveExerciseCard = ({ exercise, user, onFilterAndSortChange, onRemoveSaved
       <Stack direction="row" alignItems="center" justifyContent="center">
         <Tooltip title={"Sort by target muscle".toUpperCase()} arrow>
           <Button 
-            onClick={() => filterAndSort("target")}
+            onClick={() => sortDisplayOrderBy("target")}
             className="exercise-card-category-btn" 
             sx={{ ml: "10px", color: "#fff", background: "#ff2a2a", fontSize: "14px", borderRadius: "20px",    textTransform: "capitalize"}}
           >
@@ -111,7 +117,7 @@ const FaveExerciseCard = ({ exercise, user, onFilterAndSortChange, onRemoveSaved
         </Tooltip>
         <Tooltip title={"Sort by body part".toUpperCase()} arrow>
           <Button 
-            onClick={() => filterAndSort("bodyPart")}
+            onClick={() => sortDisplayOrderBy("bodyPart")}
             className="exercise-card-category-btn" 
             sx={{ ml: "10px", color: "#fff", background: "#ff5d5d", fontSize: "14px", borderRadius: "20px",    textTransform: "capitalize"}}
           >
@@ -120,7 +126,7 @@ const FaveExerciseCard = ({ exercise, user, onFilterAndSortChange, onRemoveSaved
         </Tooltip>
         <Tooltip title={"Sort by equipment".toUpperCase()} arrow>
           <Button 
-            onClick={() => filterAndSort("equipment")}
+            onClick={() => sortDisplayOrderBy("equipment")}
             className="exercise-card-category-btn" 
             sx={{ ml: "10px", mr: "10px", color: "#fff", background: "#ff9090", fontSize: "14px", borderRadius: "20px", textTransform: "capitalize"}}
           >
